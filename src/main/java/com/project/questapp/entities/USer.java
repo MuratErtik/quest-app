80dec8fe72b3923c483c59f6267a9b3e7be73336
Murat Ertik <dmuratertik1@gmail.com>
2025-01-19 12:13:09 -0800

    User class has been created.

---
+++ src/main/java/com/project/questapp/entities/USer.java
@@ -0,0 +1,18 @@
+package com.project.questapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class USer {
+    @Id
+    Long id;
+
+    String username;
+    String password;
+
+}
