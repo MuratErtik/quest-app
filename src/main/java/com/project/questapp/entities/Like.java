e050af615447391c7f46ca18b7116edd7b85ae89
Murat Ertik <dmuratertik1@gmail.com>
2025-01-19 12:51:41 -0800

    Foreign Key has been added.

--- src/main/java/com/project/questapp/entities/Like.java
+++ src/main/java/com/project/questapp/entities/Like.java
@@ -1,7 +1,16 @@
 package com.project.questapp.entities;
 
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -12,7 +21,24 @@ import lombok.Data;
 public class Like {
     @Id
     Long id;
-    Long postId;
-    Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    //bir user silinirse tum postlarini sil demektir
+    @JsonIgnore
+    //@JsonIgnore, bir sınıftaki bir özelliğin JSON serileştirme (serialization) ve/veya
+    //JSON'dan nesneye dönüştürme (deserialization) işlemlerinde göz ardı edilmesini sağlar.
+    Post post;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    //bir user silinirse tum postlarini sil demektir
+    @JsonIgnore
+    //@JsonIgnore, bir sınıftaki bir özelliğin JSON serileştirme (serialization) ve/veya
+    //JSON'dan nesneye dönüştürme (deserialization) işlemlerinde göz ardı edilmesini sağlar.
+    USer user;
 
 }
