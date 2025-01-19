9a7b64f5869643da59bcc565d7aa154644c311bc
Murat Ertik <dmuratertik1@gmail.com>
2025-01-19 12:51:48 -0800

    Foreign Key has been added.

--- src/main/java/com/project/questapp/entities/Post.java
+++ src/main/java/com/project/questapp/entities/Post.java
@@ -1,9 +1,18 @@
 package com.project.questapp.entities;
 
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -15,7 +24,15 @@ public class Post {
     @Id
     Long id;
 
-    Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    //bir user silinirse tum postlarini sil demektir
+    @JsonIgnore
+    //@JsonIgnore, bir sınıftaki bir özelliğin JSON serileştirme (serialization) ve/veya
+    //JSON'dan nesneye dönüştürme (deserialization) işlemlerinde göz ardı edilmesini sağlar.
+    USer user;
+
     String title;
     
     @Lob
