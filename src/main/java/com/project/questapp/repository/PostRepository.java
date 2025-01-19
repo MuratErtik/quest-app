0b92845f542f9adfc2113f7032d6bbd4521238ea
Murat Ertik <dmuratertik1@gmail.com>
2025-01-19 15:00:24 -0800

    findByUser has been added.

--- src/main/java/com/project/questapp/repository/PostRepository.java
+++ src/main/java/com/project/questapp/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.project.questapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.questapp.entities.Post;
 
 public interface PostRepository extends JpaRepository<Post,Long>{
+
+    List<Post> findByUserId(Long long1);
+    //findby ile bizim yazidigmiz degeri birlestirip arkada sorgusunu yapar!
 
 }
