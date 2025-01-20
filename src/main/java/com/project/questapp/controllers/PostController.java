d4cd0a520b927fc6f98024b596c9e2cba0009ea5
Murat Ertik <dmuratertik1@gmail.com>
2025-01-20 02:07:14 -0800

    Update and Delete feature have been added!

--- src/main/java/com/project/questapp/controllers/PostController.java
+++ src/main/java/com/project/questapp/controllers/PostController.java
@@ -3,9 +3,11 @@ package com.project.questapp.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.questapp.entities.Post;
 import com.project.questapp.requests.PostCreateRequest;
+import com.project.questapp.requests.PostUpdateRequest;
 import com.project.questapp.services.PostService;
 
 @RestController
@@ -43,5 +46,20 @@ public class PostController {
     public Post createOnePost(@RequestBody PostCreateRequest newPostCreateRequest){
         return postService.createOnePost(newPostCreateRequest);
     }
+
+
+    @PutMapping("/{postId}")
+    public Post updatePost(@PathVariable Long postId, @RequestBody PostUpdateRequest postUpdateRequest){
+        return postService.updatePostById(postId, postUpdateRequest);
+    }
+
+
+    @DeleteMapping("/{postId}")
+    public void deletePost(@PathVariable Long postId){
+        postService.deletePostById(postId);
+    }
+
+
+
 
 }
