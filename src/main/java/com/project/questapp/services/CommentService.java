e0a68f082ae52fb2bc93702ecfec646b67c93ec3
Murat Ertik <dmuratertik1@gmail.com>
2025-01-20 04:17:19 -0800

    has been created!

---
+++ src/main/java/com/project/questapp/services/CommentService.java
@@ -0,0 +1,63 @@
+package com.project.questapp.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.project.questapp.entities.Comment;
+import com.project.questapp.repository.CommentRepository;
+import com.project.questapp.repository.PostRepository;
+import com.project.questapp.repository.UserRepository;
+
+@Service
+public class CommentService {
+
+    private CommentRepository commentRepository;
+
+    private UserRepository userRepository;
+
+    private PostRepository postRepository;
+
+    public CommentService(CommentRepository commentRepository,UserRepository userRepository,PostRepository postRepository){
+
+        this.commentRepository=commentRepository;
+        this.userRepository=userRepository;
+        this.postRepository=postRepository;
+    }
+
+    public List<Comment> getAllCommentsWithParams(Optional<Long> userId, Optional<Long> postId) {
+        if (userId.isPresent() && postId.isPresent()) {
+            return commentRepository.findByUserIdAndPostId(userId.get(), postId.get());
+        }
+        else if (userId.isPresent()) {
+            return commentRepository.findByUserId(userId.get());
+        }
+        else if (postId.isPresent()) {
+            return commentRepository.findByPostid(postId.get());
+        }
+        else{
+            return commentRepository.findAll();
+        }        
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
