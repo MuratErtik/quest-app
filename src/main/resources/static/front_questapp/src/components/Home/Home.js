6b70d4c0c74aeb0b0a577348b5d637469d80c6c3
Murat Ertik <dmuratertik1@gmail.com>
2025-01-21 08:14:13 -0800

    frontend things

--- src/main/resources/static/front_questapp/src/components/Home/Home.js
+++ src/main/resources/static/front_questapp/src/components/Home/Home.js
@@ -1,17 +1,41 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";  
 import Post from "../Post/Post";
 
-function Home(){
+function Home() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [postList, setPostList] = useState([]);
 
-    return (
-        <div>
-            Home...
-            <Post></Post>
-        </div>
-    )
+    useEffect(() => {
+        fetch("http://localhost:8080/posts")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setIsLoaded(true);
+                    setPostList(result);
+                },
+                (error) => {
+                    setIsLoaded(true);  
+                    setError(error);
+                }
+            );
+    }, []);
 
+    if (error) {
+        return <div>error!</div>;
+    } else if (!isLoaded) {
+        return <div>loading...</div>;
+    } else {
+        return (
+            <div className="container">
+                Home...
+                {postList.map(post => (
+                    <Post  title={post.title} text={post.text} />  // props olarak doğru şekilde geçirme
+                ))}
+            </div>
+        );
+    }
 }
-
 
 export default Home;
--- src/main/resources/static/front_questapp/src/components/Post/Post.js
+++ src/main/resources/static/front_questapp/src/components/Post/Post.js
@@ -1,52 +1,16 @@
-import React, {useState , useEffect} from "react";
+import React from "react";
 
 import ReactDOM from "react-dom";
 
 function Post(){
-    const [error,setError]= useState(null);
+    const {title,text}=props;
 
-    const [isLoaded,setIsLoaded]= useState(false);
-
-    const [postList,setPostList]= useState([]);
-
-    useEffect(() =>{
-        fetch("http://localhost:8080/posts")
-        .then(res=>res.json())
-        .then(
-            (result) => {
-                console.log(result);
-                setIsLoaded(true);
-                setPostList(result);
-
-            },
-            (error) => {
-                setIsLoaded(true);//kod bir sekilde calisti
-                setError(error);
-
-
-            }
-        )
-    },[])
-    if(error){
-        return <div>error!</div>
-    }
-    else if(!isLoaded){
-        return <div>loading...</div>
-
-    }
-    else{
-        return(
-            <ul>
-                {postList.map(post => (
-                    <li>
-                        {post.title} {post.text}
-                    </li>
-                ))}
-            </ul>
-            
-        )
-    }
-
+    return (
+        <div className="postContainer">
+            {title}
+            {text}
+        </div>
+    )
 }
 
 export default Post;
