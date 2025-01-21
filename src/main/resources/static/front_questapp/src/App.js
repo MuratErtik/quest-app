ab5476d285b783e3c85f7255e836c560b1570161
Murat Ertik <dmuratertik1@gmail.com>
2025-01-21 07:39:41 -0800

    v1

--- src/main/resources/static/front_questapp/src/App.js
+++ src/main/resources/static/front_questapp/src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Switch , Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home/Home';
+import USer from './components/User/USer';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path="/users/:userId"  component={USer}></Route>
-
-        </Switch>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/users/:userId" element={<USer />} />
+        </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
-
+//client side bir rooting yapar server side a gitmez singlepageapp mantigi budur
 export default App;
