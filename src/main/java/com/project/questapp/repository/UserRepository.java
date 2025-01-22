1614f01bdea8557290453e6cdc92b46dedd36669
Murat Ertik <dmuratertik1@gmail.com>
2025-01-22 06:25:50 -0800

    new Method has been added.

--- src/main/java/com/project/questapp/repository/UserRepository.java
+++ src/main/java/com/project/questapp/repository/UserRepository.java
@@ -6,6 +6,8 @@ import com.project.questapp.entities.USer;
 
 
 public interface UserRepository extends JpaRepository<USer,Long>{
+ 
+    USer findByUsername(String username);
     //jparep interface i query atmaya yarar icindeki metotlar sayesinde
 
 }
