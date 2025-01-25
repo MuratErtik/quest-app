195f2bd1321367ff951c5f8bcdd30fb5946f3dc5
Murat Ertik <dmuratertik1@gmail.com>
2025-01-25 10:41:43 -0800

    isRefreshExpired has been completed.

--- src/main/java/com/project/questapp/repository/RefreshTokenRepository.java
+++ src/main/java/com/project/questapp/repository/RefreshTokenRepository.java
@@ -1,8 +1,15 @@
 package com.project.questapp.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.project.questapp.entities.Comment;
+import com.project.questapp.entities.USer;
+
 public interface RefreshTokenRepository extends JpaRepository {
+    Optional<USer> findUserByUserId(Long userId);
     
 
 }
--- src/main/java/com/project/questapp/services/RefreshTokenService.java
+++ src/main/java/com/project/questapp/services/RefreshTokenService.java
@@ -1,20 +1,55 @@
 package com.project.questapp.services;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.project.questapp.entities.RefreshToken;
+import com.project.questapp.entities.USer;
 import com.project.questapp.repository.RefreshTokenRepository;
 
 @Service
 public class RefreshTokenService {
+    @Value("${refresh.token.expires.in}")
+    Long expireSeconds;
+
     private RefreshTokenRepository refreshTokenRepository;
 
-    public RefreshTokenService(RefreshTokenRepository refreshTokenRepository){
-        this.refreshTokenRepository=refreshTokenRepository;
+    public RefreshTokenService(RefreshTokenRepository refreshTokenRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
-    public boolean isRefreshExpired(RefreshToken refreshToken){
-        return false;
+    public boolean isRefreshExpired(RefreshToken refreshToken) {
+        return refreshToken.getExpiryDate().before(new Date());
+    }
+
+    @SuppressWarnings("unchecked")
+    public String createRefreshToken(Optional<USer>  user) {
+        RefreshToken refreshToken = new RefreshToken();
+
+        user = refreshTokenRepository.findUserByUserId(user.get().getId());
+
+        if (user.isPresent()) {
+            
+            refreshToken.setExpiryDate((java.sql.Date) Date.from(Instant.now().plusSeconds(expireSeconds)));
+            refreshTokenRepository.save(refreshToken);
+            return refreshToken.getToken();
+
+        }
+        else{
+            refreshToken.setUser(user.get());
+            refreshToken.setToken(UUID.randomUUID().toString());
+            refreshToken.setExpiryDate((java.sql.Date) Date.from(Instant.now().plusSeconds(expireSeconds)));
+            refreshTokenRepository.save(refreshToken);
+            return refreshToken.getToken();
+
+        }
+
+
     }
 
 }
